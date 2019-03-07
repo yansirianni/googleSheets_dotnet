9f24dcfe3b3aee2336671667d749c0740a0fe71f
Yan Sirianni <yan@take.net>
2019-03-07 11:40:45 -0300

    create examples

---
+++ Google Sheets API .NET Quickstart/program.cs
@@ -0,0 +1,24 @@
+﻿using Google_Sheets_API_.NET_Quickstart.Read;
+using Google_Sheets_API_.NET_Quickstart.Write;
+
+namespace SheetsQuickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var read = new simpleReadExample();
+            read.readData();
+
+            //var insert = new insertExample();
+            //insert.insertData();
+
+            //var update = new updateExample();
+            //update.updateData();
+
+            //var delete = new deleteExample();
+            //delete.deleteData();
+
+        }
+    }
+}
